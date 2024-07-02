e8e2bc6c443f07b7a8087c9ce351a1666ed31929
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-07-02 17:22:36 -0300

    variable environments

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,13 @@
 import styles from './Dashboard.module.css'
+import Head from 'next/head'
 
 export default function Dashboard(){
     return (
         <div className={styles.container    }>
-            
+            <Head>
+                <title>Painel de Tarefas</title>
+            </Head>
+            <h1>Painel</h1>
         </div>
     )
 }
