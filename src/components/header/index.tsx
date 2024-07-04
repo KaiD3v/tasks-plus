9570fd9293799c1b2ead325bad1f47a0478a5821
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-07-04 08:34:23 -0300

    guard dashboard route for only authenticated users

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import styles from "./Header.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,31 @@
-import styles from './Dashboard.module.css'
-import Head from 'next/head'
+import { GetServerSideProps } from "next";
+import styles from "./Dashboard.module.css";
+import Head from "next/head";
 
-export default function Dashboard(){
-    return (
-        <div className={styles.container    }>
-            <Head>
-                <title>Painel de Tarefas</title>
-            </Head>
-            <h1>Painel</h1>
-        </div>
-    )
+import { getSession } from "next-auth/react";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Painel de Tarefas</title>
+      </Head>
+      <h1>Painel</h1>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({req})
+  if(!session?.user) {
+    return {
+        redirect: {
+            destination: '/',
+            permanent: false
+        }
+    }
+  }  
+  return {
+    props: {}
+  };
+};
