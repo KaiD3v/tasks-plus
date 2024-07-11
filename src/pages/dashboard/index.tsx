25cb95d37597886cda856f4cb2f667c3260fef5b
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-07-11 16:43:39 -0300

    Dashboard pt.02

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 
 import { getSession } from "next-auth/react";
 import { Textarea } from "../../components/textarea";
+import { FiShare2 } from "react-icons/fi" 
+import { FaTrash } from "react-icons/fa";
 
 export default function Dashboard() {
   return (
@@ -25,12 +27,42 @@ export default function Dashboard() {
                         <input type="checkbox" className={styles.checkbox}/>
                         <label>Deixar a tarefa pública?</label>
                     </div>
-
                     <button className={styles.button} type="submit">
                         Registrar
                     </button>
                 </form>
             </div>
+        </section>
+        <section className={styles.taskContainer}>
+          <h1>Minhas Tarefas</h1>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>Example task</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140"/>
+              </button>
+            </div>
+          </article>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>Example task</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140"/>
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </div>
