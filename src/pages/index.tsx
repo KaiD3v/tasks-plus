c5af2ba85883d23f8783726f230cb203aaad07e5
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-08-11 00:42:50 -0300

    add server side props at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,18 @@ import styles from "../styles/Home.module.css";
 import Image from "next/image";
 
 import heroImg from "../../public/assets/hero.png";
+import { GetStaticProps } from "next";
+import { collection, getDoc, getDocs } from "firebase/firestore";
+import { db } from "../services/firebaseConnection";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+type HomeProps = {
+  posts: number;
+  comments: number;
+};
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -25,15 +33,35 @@ export default function Home() {
         <h1 className={styles.title}>
           Sistema feito para você organizar <br />seus estudos e tarefas
         </h1>
-      <div className={styles.infoContent}>
-        <section className={styles.box}>
-          <span>+12 posts</span>
-        </section>
-        <section className={styles.box}>
-          <span>+90 Comentários</span>
-        </section>
-      </div>
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>
+              +{posts} posts
+            </span>
+          </section>
+          <section className={styles.box}>
+            <span>
+              +{comments} Comentários
+            </span>
+          </section>
+        </div>
       </main>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tasks");
+
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0
+    },
+    revalidate: 60 * 60 * 24
+  };
+};
